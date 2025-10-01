bf18a2d70aeb457e166ce9781af246fc6552a718
Andrei-Gibbini <andreigibbini52@gmail.com>
2025-10-01 02:29:45 -0300

    Instalando react-icons para ícones de contato e redes sociais. Configurando a rota da página Contato na main.

--- SPHC/src/main.tsx
+++ SPHC/src/main.tsx
@@ -1,32 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Error from './routes/Error/index.tsx'
-import PaginaInicial from './routes/PaginaInicial/index.tsx'
-import Lembretes from './routes/Lembretes/index.tsx'
-import Tutorial from './routes/Tutorial/index.tsx'
-import DetalheTutorial from './components/DetalheTutorial/DetalheTutorial.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./routes/Error/index.tsx";
+import PaginaInicial from "./routes/PaginaInicial/index.tsx";
+import Lembretes from "./routes/Lembretes/index.tsx";
+import Tutorial from "./routes/Tutorial/index.tsx";
+import DetalheTutorial from "./components/DetalheTutorial/DetalheTutorial.tsx";
+import Contato from "./routes/Contato/index.tsx";
 
 const router = createBrowserRouter([
   {
-    path: "/", element: <App />,
+    path: "/",
+    element: <App />,
     errorElement: <Error />,
     children: [
       { path: "/", element: <PaginaInicial /> },
+      { path: "/contato", element: <Contato /> },
       { path: "/tutorial", element: <Tutorial /> },
       { path: "/tutorial/:titulo", element: <DetalheTutorial /> },
       { path: "/lembretes", element: <Lembretes /> },
-    ]
-  }
-])
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
-
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-
-)
-
-
+  </StrictMode>
+);
