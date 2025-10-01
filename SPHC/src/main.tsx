8fab55743d5a4a8c493a76515c8fc8f1dbeaf0bb
devbelasp <isa.bela360@hotmail.com>
2025-10-01 01:54:36 -0300

    Criando a página inicial do Suporte HC e  página Error. Configurando o roteamento principal da aplicação usando React Router DOM.

--- SPHC/src/main.tsx
+++ SPHC/src/main.tsx
@@ -1,9 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './routes/Error/index.tsx'
+import PaginaInicial from './routes/PaginaInicial/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <PaginaInicial /> },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
+
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
+
 )
+
+
